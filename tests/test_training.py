486bd84d8b07d35fa342d9d1223c4a36960338e7
JonatanHauge <johauge@hotmail.com>
2024-01-08 12:53:03 +0100

    test

--- tests/test_training.py
+++ tests/test_training.py
@@ -6,7 +6,8 @@ import os
 def test_training():
     #train_pl(1e-3, 1, "models/", False)
     assert os.path.exists('models/trained_model.pt')
-
+    
+    os.environ["WANDB_MODE"] = "offline"
     exit_code = os.system('make train train_params=test_params')
 
     assert exit_code == 0, 'Training failed'
